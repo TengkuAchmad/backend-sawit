364b8ec244760523f09afa77b303e7a83806d07e
= <achmad.tengku@gmail.com>
2025-01-14 22:49:27 +0700

    update: user route without middlewares

--- app/routes/user.route.js
+++ app/routes/user.route.js
@@ -24,9 +24,9 @@ router.get("/user-management/get/admin/all", authenticateToken, user_controllers
 
 router.get("/user-management/get/admin/:id", authenticateToken, user_controllers.getOneAdmin);
 
-router.post("/user-management/send/otp", authenticateToken, user_controllers.sendOTP);
+router.post("/user-management/send/otp", user_controllers.sendOTP);
 
-router.post("/user-management/verify/otp", authenticateToken, user_controllers.verifyOTP);
+router.post("/user-management/verify/otp", user_controllers.verifyOTP);
 
 router.delete("/user-management/delete/admin/all", authenticateToken, user_controllers.deleteAllAdmin);
 
@@ -38,6 +38,6 @@ router.delete("/user-management/delete/:id", authenticateToken,  user_controller
 
 router.put("/user-management/update/:id", authenticateToken, user_controllers.updateOne);
 
-router.put("/user-management/update/pass/:id", authenticateToken, user_controllers.updatePass);
+router.put("/user-management/update/pass/:id", user_controllers.updatePass);
 
 module.exports = router;
